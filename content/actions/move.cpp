2c49f6bfd6a7fda468282ba91292ddb996a9c5d2
Owen Thomson <othomson6110@gmail.com>
2023-04-17 20:59:09 -0500

    change direction

--- content/actions/move.cpp
+++ content/actions/move.cpp
@@ -9,17 +9,6 @@ Move::Move(Vec direction) : direction{direction} {}
 
 Result Move::perform(Engine& engine) {
     Vec position = actor->get_position() + direction;
-
-    // bool is_even = (position.x + position.y) % 2 == 0;
-
-    // for (auto& [p, door] : engine.dungeon.doors) {
-    //     if (is_even) {
-    //         door.open();
-    //     } else {
-    //         door.close();
-    //     }
-    // }
-
     Tile& tile = engine.dungeon.tiles(position);
     if (tile.is_wall()) {
         return failure();
@@ -27,6 +16,7 @@ Result Move::perform(Engine& engine) {
     if (tile.is_door()) {
         return failure();
     }
+    actor->change_direction(direction);
     actor->move_to(position);
     return success();
 }
