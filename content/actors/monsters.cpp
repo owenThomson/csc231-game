af02ac7bfcb1779976ef10d77ba2139b51891b69
Owen Thomson <othomson6110@gmail.com>
2023-04-28 09:06:34 -0500

    monsters

--- content/actors/monsters.cpp
+++ content/actors/monsters.cpp
@@ -1,1 +1,18 @@
 #include "monsters.h"
+
+#include "none.h"
+#include "rest.h"
+
+namespace Monsters {
+std::unique_ptr<Action> default_behavior(Engine&, Monster&) {
+    return std::make_unique<Rest>();
+}
+
+constexpr int default_speed{8};
+
+MonsterType goblin() {
+    int health = 2;
+    return {"goblin", default_speed, health, std::make_shared<None>(),
+            default_behavior};
+}
+}  // namespace Monsters
--- content/actors/monsters.h
+++ content/actors/monsters.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "action.h"
+#include "monstertype.h"
+
 namespace Monsters {
-    
+std::unique_ptr<Action> default_behavior(Engine& engine, Monster& monster);
+MonsterType gobline();
+MonsterType skeleton();
 
-
-}
+}  // namespace Monsters
